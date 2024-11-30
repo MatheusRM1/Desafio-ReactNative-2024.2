f1ff9bbbee79e2ecd90303af7ed18e8c14259209
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2024-11-29 21:29:52 -0300

    header corrigido

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import TabRoutes from './tab.routes';
-import Header from '../components/Header';
 
 export default function Routes(){
     return(
