39fa09e3783c40b872fe7216f6636d90deb1fd37
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2024-12-18 16:05:16 -0300

    correção no margin do pvu

--- src/screens/Pvu.js
+++ src/screens/Pvu.js
@@ -28,13 +28,27 @@ export default function VisualizacaoIndividual() {
         </View>
         <View style={estilo.bloco}>
           <Text style={estilo.letra}>
-            A nossa fortaleza vence qualquer ciúme Então por gentileza meu bebê
-            Deita com a luz acesa, põe aquele perfume Que hoje a sobremesa é
-            você Se o negócio é brigar, discutir relação Sobe de patamar, muda o
-            seu coração Eu não vou aceitar sua provocação É melhor evitar pelo
-            sim pelo não Só pra me enganar bota o feijão no fogo e Para de
-            ferver Tira esse brilho genioso do olhar Que eu cheguei tarde, mas o
-            coro vai comer.
+            Your blades are sharpened with precision Flashing your favorite
+            point of view I know you're waiting in the distance Just like you
+            always do Just like you always do Already pulling me in Already
+            under my skin And I know exactly how this ends I let you cut me
+            open, just to watch me bleed Gave up who I am for who you wanted me
+            to be Don't know why I'm hoping for what I won't receive Falling for
+            the promise of the emptiness machine Going around like a revolver
+            It's been decided how we lose 'Cause there's a fire under the altar
+            I keep on lying to I keep on lying to Already pulling me in Already
+            under my skin And I know exactly how this ends I let you cut me
+            open, just to watch me bleed Gave up who I am for who you wanted me
+            to be Don't know why I'm hoping for what I won't receive Falling for
+            the promise of the emptiness machine I only wanted to be part of
+            something I only wanted to be part of, part of I only wanted to be
+            part of something I only wanted to be part of, part of I only wanted
+            to be part of something I only wanted to be part I let you cut me
+            open, just to watch me bleed Gave up who I am for who you wanted me
+            to be Don't know why I'm hoping, so fucking naive Falling for the
+            promise of the emptiness machine The emptiness machine (I only
+            wanted to be part of something) The emptiness machine (I only wanted
+            to be part of) The emptiness machine
           </Text>
         </View>
       </View>
@@ -87,8 +101,9 @@ const estilo = StyleSheet.create({
   Image: {
     width: "80%",
     height: 300,
+    objectFit: "cover",
     borderRadius: 50,
     marginBottom: 30,
-    marginTop: 20,
+    marginTop: 5
   },
 });
